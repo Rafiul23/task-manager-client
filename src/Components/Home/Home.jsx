8686facfa0891410839196ecba45d7e3dd9a2941
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-12-21 16:59:55 +0600

    added home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,14 @@
-
+import tasks from '../../assets/task.png'
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='flex flex-col md:flex-row'>
+            <img src={tasks} className='lg:w-full md:w-1/2' alt="" />
+            <div className='text-purple-600 my-auto w-full md:w-1/2 md:text-left text-center'>
+                <h2 className='text-3xl font-bold'>Enlist and Manage your Tasks at Task Manager</h2>
+                <p className='text-xl'>It is easy and useful.</p>
+                <button className='btn bg-purple-600 text-white mt-4'>Get started</button>
+            </div>
         </div>
     );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar bg-purple-600 text-white">
+        <div className="navbar bg-purple-600 lg:text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
